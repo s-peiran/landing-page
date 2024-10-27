b3b6af30a48058e738bcaf178f9b111049e4b4c1
s-peiran <she_peiran@u.nus.edu>
2024-10-27 23:42:53 +0800

    basic landing page

---
+++ src/components/header.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export const Header = () => {
+	const router = useRouter();
+	const handleNavigation = (route: string) => {
+		router.push(route);
+	};
+
+	return (
+		<header className="flex items-center bg-[#0F1421] min-h-[70px]">
+			<nav
+				className="px-5 py-2 bg-white ml-5 rounded-sm cursor-pointer font-semibold text-[#0F1421]"
+				onClick={() => handleNavigation("/")}
+			>
+				She peiran
+			</nav>
+			<nav className="ml-auto flex justify-between gap-x-5 pr-5">
+				<ul className="px-5 py-2 bg-blue-300 rounded-sm cursor-pointer">
+					About
+				</ul>
+				<ul className="px-5 py-2 bg-blue-300 rounded-sm cursor-pointer">
+					Projects
+				</ul>
+			</nav>
+		</header>
+	);
+};
